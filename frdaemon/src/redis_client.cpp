937e750fb33a2f6486f5b018784d4f6770cb0e6e
oleksa <shutt10@gmail.com>
2017-06-26 15:58:37 +0300

    * redis var names changed

--- frdaemon/src/redis_client.cpp
+++ frdaemon/src/redis_client.cpp
@@ -97,15 +97,15 @@ bool RedisClient::get_configuration()
 		}
 	}
 
-	config_ftp_url			= redis_.hget(config_key, "ftp");
-	config_camera_url		= redis_.hget(config_key, "camera");
+	config_ftp_url			= redis_.hget(config_key, "Ftp");
+	config_camera_url		= redis_.hget(config_key, "Camera");
 	config_camera_number	= redis_.hget(config_key, "CameraNumber");
-	config_channel			= redis_.hget(config_key, "channel");
+	config_channel			= redis_.hget(config_key, "Channel");
 
 	//config_db_host = redis_.get("frd:db_host");
 	//config_db_name = redis_.get("frd:db_name");
-	config_db_host			= redis_.hget(config_key, "db_host");
-	config_db_name			= redis_.hget(config_key, "db_name");
+	config_db_host			= redis_.hget(config_key, "DbHost");
+	config_db_name			= redis_.hget(config_key, "DbName");
 
 	return true;
 }
